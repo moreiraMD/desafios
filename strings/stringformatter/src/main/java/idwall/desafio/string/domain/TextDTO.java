3e220a324c1db169c77c60a511f6bd8d50bc883a
Márcio Daniel <moreiramarciodaniel@gmail.com>
2019-09-21 14:13:49 -0300

    Add controller and domain

---
+++ strings/stringformatter/src/main/java/idwall/desafio/string/domain/TextDTO.java
@@ -0,0 +1,13 @@
+package idwall.desafio.string.domain;
+
+public class TextDTO {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
